8fb619fb54ab85596f521f4fc2a2e4fe493791f9
rediet <redietmesfinofficial@gmail.com>
2024-11-14 12:14:12 +0300

    clean up

--- my-project/src/pages/projects.jsx
+++ my-project/src/pages/projects.jsx
@@ -1,9 +1,8 @@
-// src/pages/projects.jsx
-import React from "react";
+
 import { useTranslation } from 'react-i18next';
 
 const Projects = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const projectsData = [
     {
       id: 1,
