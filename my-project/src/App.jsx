7225563516b269bafd9d27ead4677382f3487cc2
nani020 <oumernani@gmail.com>
2025-02-13 10:30:32 +0300

    translation

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/about.jsx";
 import Contact from "./pages/contact.jsx";
 import Service from "./pages/service.jsx";
 import Projects from "./pages/projects.jsx";
+import Support from "../src/pages/support.jsx";
 import ScrollToTop from "./compononets/scroll_to_top.jsx";
 import './App.css';
 
@@ -21,6 +22,8 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/service" element={<Service />} />
             <Route path="/projects" element={<Projects />} />
+            <Route path="/support" element={<Support/>}/>
+            
           </Route>
           <Route path="*" element={<NoPage />} />
         </Routes>
